d329c07d29b6220e0c0e6d342fd9c7aabec5ae0f
MarinaInCodeLand <jakov.marina@gmail.com>
2025-05-31 22:15:12 +0200

    react router dom fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,28 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { BrowserRouter as Router } from "react-router";
+// import './index.css';
+// import App from './App.jsx';
+// import "./Fonts/F1/Formula1-Regular_web_0.ttf"
+// import "./Fonts/F1/Formula1-Bold_web_0.ttf"
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </StrictMode>,
+// )
+
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router";
+import { HashRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
 import "./Fonts/F1/Formula1-Regular_web_0.ttf"
 import "./Fonts/F1/Formula1-Bold_web_0.ttf"
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
